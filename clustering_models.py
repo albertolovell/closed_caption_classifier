7e079d6cba6e754e7d8d6a11f7e38d657e0078e6
Ubuntu <ubuntu@ip-172-31-36-54.ec2.internal>
2018-11-16 22:50:29 +0000

    plot clusters.py

--- clustering_models.py
+++ clustering_models.py
@@ -14,8 +14,7 @@ from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from scipy.spatial.distance import cdist
 from sklearn.cluster import KMeans
-from sklearn.decomposition import TruncatedSVD
-from sklearn.decomposition import NMF, LatentDirichletAllocation
+from sklearn.decomposition import NMF, LatentDirichletAllocation, TruncatedSVD
 
 
 
@@ -24,4 +23,30 @@ def vectorize_text(doc_list):
     
     vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), lowercase=True) 
     tfidf_model = vectorizer.fit_transform(doc_list)
+    return tfidf_model
+    
+def svd(doc_list):
+    
+    model = vectorize_text(doc_list)
+    
+    svd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+    clf = svd.fit_transform(model) 
+    
+    return clf
+    
+def find_elbow(doc_list):
+    
+    distortions = []
+    K = range(1,10)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k, n_jobs=2)
+        kmeanModel.fit(clf)
+        distortions.append(sum(np.min(cdist(clf, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / clf.shape[0])
+
+    # Plot the elbow
+    plt.plot(K, distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
 
