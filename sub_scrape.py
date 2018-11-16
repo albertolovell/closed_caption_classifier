c43b85132aa5a696fe6f5bf8878ab7edb06b4cb4
Ubuntu <ubuntu@ip-172-31-36-54.ec2.internal>
2018-11-16 06:17:00 +0000

    overnight scrape

---
+++ sub_scrape.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from urllib.request import Request, urlopen
+from socket import error as SocketError
+import errno
+import time
+
+
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
+
+
+
+def scrape_text(series):
+    
+    #use pd.series instead moving forward
+    text = []
+    for link in series:
+        
+        try:
+            req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            webpage = urlopen(req).read()
+            webpage = webpage.decode('utf-8')
+            text.append(webpage)
+        except SocketError as e:
+            if e.errno != errno.ECONNRESET:
+                raise # Not error we are looking for
+            pass # Handle error here.
+        
+    return text
+
+def scrape_from_url(cycles):
+    
+    for i in range(cycles):
+        
+        time.sleep(900)
+        
+        from_df = pd.read_csv('data/cc_recent.csv', encoding='utf-8')
+        temp_df = from_df.head(1000)
+
+        #scrape text then concat
+        text_url_series = pd.Series(temp_df['url'])
+        extracts = scrape_text(text_url_series)
+        extracts = pd.Series(extracts)
+        temp_df['text'] = extracts
+
+        head_df = pd.read_csv('data/cc_head_text.csv', encoding='utf-8')
+        head_df = pd.concat([head_df, temp_df])
+        head_df.to_csv('data/cc_head_text.csv', encoding='utf-8', index=False)
+
+        from_df = from_df.drop(from_df.index[0:1000])
+        from_df.to_csv('data/cc_recent.csv', encoding='utf-8', index=False)
+
+        head_df=None
+        temp_df=None
+        from_df=None
+        text_url_series=None
+        extracts=None
+        
+if __name__ == "__main__":
+    
+    scrape_from_url(30)
