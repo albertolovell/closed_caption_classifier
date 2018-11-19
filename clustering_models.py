fcb114f11b3a76608fa3409a5ada93a65adc9159
Ubuntu <ubuntu@ip-172-31-37-231.ec2.internal>
2018-11-19 20:38:36 +0000

    latent semantic analysis

--- clustering_models.py
+++ clustering_models.py
@@ -1,20 +1,52 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import datetime
+import string
+from string import digits
+import collections
 import scipy.stats as scs
 import cc_pipeline as P
+import time
+import random
+import pickle
 
+#sentiment and language
 import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import spacy
 import vaderSentiment
 from langdetect import detect
+from gensim.models import Word2Vec
+from gensim import corpora
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA
 from sklearn.pipeline import Pipeline
 from scipy.spatial.distance import cdist
 from sklearn.cluster import KMeans
-from sklearn.decomposition import NMF, LatentDirichletAllocation, TruncatedSVD
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+from gensim.models.ldamodel import LdaModel
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import chi2
+import knee_locator
+
+#plotting
+from bokeh.plotting import figure, show, output_file, output_notebook, ColumnDataSource
+from bokeh.models import HoverTool, BoxSelectTool
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
+import pyLDAvis.sklearn
+import pyLDAvis.gensim as gensimvis
+import pyLDAvis
+
+nltk.download('punkt')
 
 
 
@@ -52,24 +84,61 @@ def find_elbow(doc_list):
     plt.xlabel('k')
     plt.ylabel('Distortion')
     plt.title('The Elbow Method showing the optimal k')
+    plt.savefig('data/kmeans_elbow.png')
     plt.show()
 
-def KMeans_PCA_plot(doc_list, num_clusters=3, n_jobs=4):
+
+def kernel_pca(text, components=5):
     
-    clf = svd(doc_list)
+    tfidf_model = vectorize_text(text)
+
+    kpca = KernelPCA(n_components=components, kernel='rbf', gamma=15)
+    X_kpca = kpca.fit_transform(tfidf_model)
+
+
+    plt.figure()
+    plt.title('TFIDF - KernelPCA')
+    plt.scatter(X_kpca[:, 0], X_kpca[:, 1], c='rgb')
+    plt.savefig('data/kernel_pca.png')
+    plt.show()
     
-    km = KMeans(n_clusters=num_clusters, n_jobs=4) 
-    km.fit(clf)
+    
+def standard_pca(text, components=4):
 
-    clusters = km.labels_.tolist()
+    tfidf_model = vectorize_text(text)
+    tfidf_dense = tfidf_model.todense()
 
-    pca = PCA(n_components=2).fit(clf)
-    data2D = pca.transform(clf)
-    plt.scatter(data2D[:,0], data2D[:,1], c=clusters, alpha=0.1)
+    pca = PCA(n_components=components)
+    data2D = pca.fit_transform(tfidf_dense)
 
-    centers2D = pca.transform(km.cluster_centers_)
-
-    plt.scatter(centers2D[:,0], centers2D[:,1], 
-                marker='x', s=200, linewidths=3, c='r')
-    plt.title('(SVD REDUCED) Kmeans Clusters')
+    #this array is one dimesional so we plot using
+    plt.scatter(data2D[:,0], data2D[:,1], c='rgb', alpha=0.1)
+    plt.title('PCA Reduction')
+    plt.savefig('data/standard_pca.png')
     plt.show() 
+    
+    
+def latent_semantic_analysis(text, components=100):
+    
+    tfidf_model = vectorize_text(text)
+
+    svd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+    clf = svd.fit_transform(tfidf_model) 
+
+    #this array is one dimesional so we plot using
+    plt.scatter(clf[:,0], clf[:,1], c='rgb', alpha=0.1)
+    plt.title('Truncated SVD Reduction')
+    plt.savefig('data/lsa.png')
+    plt.show() 
+
+    
+    
+    
+    
+if __name__=="__main__":
+    
+    df = pd.read_csv('data/testenglish.csv', encoding='utf-8')
+    text = df['cleaned'].values
+    text = text.tolist()
+    
+    
