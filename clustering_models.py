8eb269ed914c9b74d196e058e829427f7a3e3155
Ubuntu <ubuntu@ip-172-31-37-231.ec2.internal>
2018-11-20 00:15:52 +0000

    wordcloud

--- clustering_models.py
+++ clustering_models.py
@@ -45,6 +45,7 @@ from PIL import Image
 import pyLDAvis.sklearn
 import pyLDAvis.gensim as gensimvis
 import pyLDAvis
+import umap
 
 nltk.download('punkt')
 
@@ -53,7 +54,7 @@ nltk.download('punkt')
 
 def vectorize_text(doc_list):
     
-    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), lowercase=True) 
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 3), lowercase=True) 
     tfidf_model = vectorizer.fit_transform(doc_list)
     
     return tfidf_model
@@ -86,6 +87,30 @@ def find_elbow(doc_list):
     plt.title('The Elbow Method showing the optimal k')
     plt.savefig('data/kmeans_elbow.png')
     plt.show()
+    
+def plot_kmeans(df, text, clusters=5):
+    
+    temp_df = df
+    
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 3), lowercase=True) 
+    tfidf_model = vectorizer.fit_transform(text)
+    
+    kmeans = KMeans(n_clusters=clusters, n_init = 5, n_jobs = -1)
+    km = kmeans.fit_transform(tfidf_model)
+    
+    #common words amongst each cluster
+    common = []
+    words = vectorizer.get_feature_names()
+    common_words = kmeans.cluster_centers_.argsort()[:,-1:-11:-1]
+    for num, centroid in enumerate(common_words):
+        common.append(str(num) + ' : ' + ', '.join(words[word] for word in centroid))
+    
+    kmeans_df = temp_df
+    kmeans_df['cluster'] = kmeans.labels_
+    kmeans_df.to_csv('data/all_kmeans_clusters.csv', encoding='utf-8', index=False)
+    
+    return common
+    
 
 
 def kernel_pca(text, components=5):
@@ -118,11 +143,11 @@ def standard_pca(text, components=4):
     plt.show() 
     
     
-def latent_semantic_analysis(text, components=100):
+def latent_semantic_analysis(text, components=5):
     
     tfidf_model = vectorize_text(text)
 
-    svd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+    svd = TruncatedSVD(n_components=components, n_iter=7, random_state=42)
     clf = svd.fit_transform(tfidf_model) 
 
     #this array is one dimesional so we plot using
@@ -130,6 +155,78 @@ def latent_semantic_analysis(text, components=100):
     plt.title('Truncated SVD Reduction')
     plt.savefig('data/lsa.png')
     plt.show() 
+    
+def show_important_words(text):
+    
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 3), lowercase=True) 
+    tfidf_model = vectorizer.fit_transform(text)
+    
+    svd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
+    clf = svd.fit_transform(tfidf_model) 
+    
+    terms = vectorizer.get_feature_names()
+
+    for i, comp in enumerate(svd.components_):
+    
+        terms_comp = zip(terms, comp)
+        sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:7]
+        print("Topic "+str(i)+": ")
+        for t in sorted_terms:
+            print(t[0])
+            print(" ")
+    
+    return
+    
+def plot_topics(text, components=5):
+    
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 3), lowercase=True) 
+    tfidf_model = vectorizer.fit_transform(text)
+    svd = TruncatedSVD(n_components=components, n_iter=7)
+    clf = svd.fit_transform(tfidf_model) 
+    
+    X_topics = svd.fit_transform(tfidf_model)
+    embedding = umap.UMAP(n_neighbors=150, min_dist=0.5, random_state=12).fit_transform(X_topics)
+
+    plt.scatter(embedding[:, 0], embedding[:, 1], 
+    c = 'rgb',
+    s = 10, # size
+    edgecolor='none'
+    )
+    plt.show()
+    
+    
+def plot_wordcloud(text_string):
+    
+    # Create and generate a word cloud image:
+    wordcloud = WordCloud().generate(text_string)
+
+    # Display the generated image:
+    plt.figure(figsize=(10, 30))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.title('Topic 0')
+    plt.axis("off")
+    plt.show()
+    
+    
+    #OR blue/white
+    
+    all_words = []
+    for line in df_paulry['no_pauls']: # try 'tokens'
+        all_words.extend(line)
+
+        # create a word frequency dictionary
+        wordfreq = Counter(all_words)
+        # draw a Word Cloud with word frequencies
+        wordcloud = WordCloud(width=900,
+                              height=500,
+                              max_words=500,
+                              max_font_size=100,
+                              relative_scaling=0.5,
+                              colormap='Blues', normalize_plurals=True).generate_from_frequencies(wordfreq)
+        plt.figure(figsize=(17,14))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.show()
 
     
     
